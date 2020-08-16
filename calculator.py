056cf3a91dc034a73a4da50f655702588169f20a
bahati365 <damien.bahati@mail.utoronto.ca>
2020-08-16 14:08:24 -0400

    added a comment

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def add(num1, num2):
 # function to subtract two numbers.
 def subtract(num1, num2):
     return num1 - num2
-
+#home to me 
 
 # function to multiply two numbers.
 def multiply(num1, num2, ):
